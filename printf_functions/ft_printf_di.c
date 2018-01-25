b35140e057f79f3ee89fc3ca2f0b8755a8f3b094
Thomas BLEUSE <tbleuse@e3r7p22.42.fr>
2018-01-25 14:51:14 +0100

    modif di

--- printf_functions/ft_printf_di.c
+++ printf_functions/ft_printf_di.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 10:25:14 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/01/24 17:45:25 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/01/25 14:50:54 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,33 @@ static int	ft_printf_di_rest(char *str, int *info)
 	return (0);
 }
 
+static int	ft_modif_minus(char **str, int **info)
+{
+	char	*tmp;
+
+	tmp = *str;
+	if (!(*str = ft_strdup(&((*str)[1]))))
+		return (0);
+	(*info)[0] = 2;
+	ft_strdel(&tmp);
+	return (1);
+}
+
 static int	ft_printf_di_second(char **str, int *info)
 {
 	int		count;
 
 	count = 0;
-	if (info[1] == 1)
-		if (!ft_addncharbefore(info[6] - (int)ft_strlen(*str), '0', str))
-			return (0);
-	if (info[1] == -1)
-		if (!ft_addncharbefore(info[6] - (int)ft_strlen(*str), ' ', str))
-			return (0);
-	if (info[3] != -1 && (*str)[0] != '-')
+	if (!ft_addncharbefore(info[6] - (int)ft_strlen(*str), '0', str))
+		return (0);
+	if (info[3] != -1 && info[0] != 2)
 		if (!ft_addncharbefore(1, '+', str))
 			return (0);
-	if (info[4] != -1 && (*str)[0] != '-')
+	if (info[4] != -1 && info[0] != 2)
 		if (!ft_addncharbefore(1, ' ', str))
+			return (0);
+	if (info[0] == 2)
+		if (!ft_addncharbefore(1, '-', str))
 			return (0);
 	if (info[2] == -1)
 		count += ft_printf_di_rest(*str, info);
@@ -75,5 +86,8 @@ int			ft_printf_di(va_list ap, int *info)
 		str = ft_lltoa((long long)va_arg(ap, unsigned long long));
 	if (!str)
 		return (0);
+	if (str[0] == '-')
+		if (!(ft_modif_minus(&str, &info)))
+			return (0);
 	return (ft_printf_di_second(&str, info));
 }
