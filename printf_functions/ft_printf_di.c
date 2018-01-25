75d5c5929fbf75dde1f4da9e2ce281d8a1c99477
Thomas BLEUSE <tbleuse@e3r7p22.42.fr>
2018-01-25 15:16:55 +0100

    changed di3

--- printf_functions/ft_printf_di.c
+++ printf_functions/ft_printf_di.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 10:25:14 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/01/25 15:02:46 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/01/25 15:16:26 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,15 @@ static int	ft_printf_di_rest(char *str, int *info)
 		if (info[1] == -1)
 			ft_printnchar(info[5] - len, ' ');
 		else
-			ft_printnchar(info[5] - len, '0');
+		{
+			if (info[3] != -1 && info[0] != 2)
+			{
+				write(1, "+", 1);
+				ft_printnchar(info[5] - len - 1, '0');
+			}
+			else
+				ft_printnchar(info[5] - len, '0');
+		}
 		return (info[5] - len);
 	}
 	return (0);
@@ -47,14 +55,14 @@ static int	ft_printf_di_second(char **str, int *info)
 	count = 0;
 	if (!ft_addncharbefore(info[6] - (int)ft_strlen(*str), '0', str))
 		return (0);
-	if (info[3] != -1 && info[0] != 2)
-		if (!ft_addncharbefore(1, '+', str))
-			return (0);
 	if (info[4] != -1 && info[0] != 2)
 		if (!ft_addncharbefore(1, ' ', str))
 			return (0);
 	if (info[0] == 2)
 		if (!ft_addncharbefore(1, '-', str))
+			return (0);
+	if (info[3] != -1 && info[0] != 2 && info[2] != -1)
+		if (!ft_addncharbefore(1, '+', str))
 			return (0);
 	if (info[2] == -1)
 		count += ft_printf_di_rest(*str, info);
