1f1c4b4f5bc53b503d08b1f7fcefc7394a9f142c
Thomas BLEUSE <tbleuse@e3r12p19.42.fr>
2018-02-01 12:08:09 +0100

    hexa changed

--- printf_functions/ft_printf_mx.c
+++ printf_functions/ft_printf_mx.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 10:29:26 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/01/20 11:52:34 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/02/01 12:03:44 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,49 @@
 
 static int		ft_printf_mx_rest(char *str, int *info)
 {
-	int			len;
+	int		len;
+	int		to_add;
 
+	to_add = 0;
 	len = (int)ft_strlen(str);
 	if (info[5] > len)
 	{
 		if (info[1] == -1)
 			ft_printnchar(info[5] - len, ' ');
 		if (info[1] != -1)
+		{
+			if (info[0] != -1)
+			{
+				write(1, "0X", 2);
+				to_add = 2;
+				info[0] = -1;
+				info[5] -= 2;
+			}
 			ft_printnchar(info[5] - len, '0');
-		return (info[5] - len);
+		}
+		return (info[5] - len + to_add);
 	}
 	return (0);
 }
 
-static char		*ft_printf_mx_modif_str(char *str)
-{
-	int			i;
-
-	i = -1;
-	while (str[++i] != '\0')
-		if (str[i] >= 'a' && str[i] <= 'f')
-			str[i] = str[i] - 32;
-	return (str);
-}
-
-static int		ft_addomxbefore(char **str)
-{
-	char		*tmp;
-	int			len;
-
-	if (!str || !(*str))
-		return (0);
-	len = ft_strlen(*str);
-	tmp = *str;
-	if (!(*str = malloc(len + 3)))
-		return (0);
-	(*str)[0] = '0';
-	(*str)[1] = 'X';
-	(*str)[len + 2] = '\0';
-	while (len >= 0)
-	{
-		(*str)[len + 2] = tmp[len];
-		len--;
-	}
-	ft_strdel(&tmp);
-	return (1);
-}
-
 int				ft_printf_mx(unsigned long long nb, int *info)
 {
-	int		count;
-	char	*str;
+	int			count;
+	char		*str;
 
 	count = 0;
-	str = ft_printf_mx_modif_str(ft_ulltoa_base(nb, 16));
+	str = ft_ulltoa_base(nb, 16);
 	if (!ft_addncharbefore(info[6] - (int)ft_strlen(str), '0', &str))
 		return (0);
-	if (info[0] != -1)
-		if (!ft_addomxbefore(&str))
-			return (0);
 	if (info[2] == -1)
 		count += ft_printf_mx_rest(str, info);
+	if (info[0] != -1)
+	{
+		write(1, "0X", 2);
+		count += 2;
+		info[0] = -1;
+		info[5] -= 2;
+	}
 	ft_putstr(str);
 	count += (int)ft_strlen(str);
 	if (info[2] != -1)
--- printf_functions/ft_printf_x.c
+++ printf_functions/ft_printf_x.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 10:29:26 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/01/20 11:50:36 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/02/01 11:59:41 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,28 @@
 static int		ft_printf_x_rest(char *str, int *info)
 {
 	int		len;
+	int		to_add;
 
+	to_add = 0;
 	len = (int)ft_strlen(str);
 	if (info[5] > len)
 	{
 		if (info[1] == -1)
 			ft_printnchar(info[5] - len, ' ');
 		if (info[1] != -1)
+		{
+			if (info[0] != -1)
+			{
+				write(1, "0x", 2);
+				to_add = 2;
+				info[0] = -1;
+				info[5] -= 2;
+			}
 			ft_printnchar(info[5] - len, '0');
-		return (info[5] - len);
+		}
+		return (info[5] - len + to_add);
 	}
 	return (0);
-}
-
-static int		ft_addoxbefore(char **str)
-{
-	char		*tmp;
-	int			len;
-
-	if (!str || !(*str))
-		return (0);
-	len = ft_strlen(*str);
-	tmp = *str;
-	if (!(*str = malloc(len + 3)))
-		return (0);
-	(*str)[0] = '0';
-	(*str)[1] = 'x';
-	(*str)[len + 2] = '\0';
-	while (len >= 0)
-	{
-		(*str)[len + 2] = tmp[len];
-		len--;
-	}
-	ft_strdel(&tmp);
-	return (1);
 }
 
 int				ft_printf_x(unsigned long long nb, int *info)
@@ -60,13 +48,15 @@ int				ft_printf_x(unsigned long long nb, int *info)
 	str = ft_ulltoa_base(nb, 16);
 	if (!ft_addncharbefore(info[6] - (int)ft_strlen(str), '0', &str))
 		return (0);
-	if (info[0] != -1)
-	{
-		if (!ft_addoxbefore(&str))
-			return (0);
-	}
 	if (info[2] == -1)
 		count += ft_printf_x_rest(str, info);
+	if (info[0] != -1)
+	{
+		write(1, "0x", 2);
+		count += 2;
+		info[0] = -1;
+		info[5] -= 2;
+	}
 	ft_putstr(str);
 	count += (int)ft_strlen(str);
 	if (info[2] != -1)
