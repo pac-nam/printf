9b5999e059b4a41585acebe3817772d9c84e8c7d
Thomas BLEUSE <tbleuse@student.42.fr>
2018-02-10 18:00:20 +0100

    all

--- printf_functions/ft_printf_mc.c
+++ printf_functions/ft_printf_mc.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 10:24:41 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/02/08 12:47:35 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/02/10 17:30:40 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int			ft_printf_mc(va_list ap, int *info)
 	wchar_t		c;
 	char		*str;
 
-	count = 0;
-	c = (wchar_t)va_arg(ap, wint_t);
+	c = va_arg(ap, wchar_t);
 	if (!(str = (char*)malloc(5)))
 		return (0);
 	str[4] = '\0';
-	count += ft_wctomb(str, c);
+	count = ft_wctomb(str, c);
 	if (info[2] == -1)
-		count += ft_printnchar(info[5] - 1, ' ');
-	ft_putstr(str);
+		count += ft_printnchar(info[5] - count, ' ');
+	write(1, str, ft_strlen(str));
 	if (info[2] != -1)
-		count += ft_printnchar(info[5] - 1, ' ');
-	return (count + 1);
+		count += ft_printnchar(info[5] - count, ' ');
+	ft_strdel(&str);
+	return (count);
 }
